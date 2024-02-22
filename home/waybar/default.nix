f7fa4740f950ae3d303946fba02303e2718a3aba
RoseHobgoblin <EarlOfMagadan@protonmail.com>
2024-02-23 12:53:15 +1300

    w

--- home/waybar/default.nix
+++ home/waybar/default.nix
@@ -7,7 +7,7 @@
 }:
 {
   #import = [ ./style.nix ];
-  xdg.configFile."waybar/style.css".text = import ./style.nix;
+  xdg.configFile."waybar/style.css".text = import "./style.nix";
   programs.waybar = {
     enable = true;
     #package = pkgs.waybar;

8fb355ce8d10651d2f072217f5c84fb5996f4250
RoseHobgoblin <earlofmagadan@Protonmail.com>
2024-05-02 13:38:34 +1200

    boot initt

--- system/boot.nix
+++ system/boot.nix
@@ -1,19 +1,23 @@
-{ config, ... }: 
+{ config, pkgs, lib, ... }: 
 {
-  #boot.loader.systemd-boot.enable = true;
-  #boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.device = "nodev";
+ boot.loader.systemd-boot.enable = true;
+ boot.loader.efi.canTouchEfiVariables = true;
+#  boot.loader.grub.efiSupport = true;
+#  boot.loader.grub.device = "nodev";
 
-  boot.initrd.verbose = false;
-  boot.consoleLogLevel = 0;
-  boot.kernelParams = [ "quiet" "udev.log_level=0" ]; 
+#
+#    boot.kernelParams = [ "quiet" "splash" ];
+#    boot.consoleLogLevel = 0;
+#    boot.initrd.verbose = false;
+#    boot.initrd.systemd.enable = true;
+  #boot.initrd.verbose = false;
+  #boot.consoleLogLevel = 0;
+  #boot.kernelParams = [ "quiet" "udev.log_level=0" ]; 
 
   boot.plymouth.enable = true;
-  boot.plymouth.theme = "bgrt"; 
-  boot.plymouth.logo = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/RoseHobgoblin/RosebloomOS/main/assets/RosebloomEmblem.png";
-    sha256 = "0xdfjhplmg5qgnjmr70y93b5fza9kpzfcxhgmkn9sz52vlc5v195";
-  }
-
+  #boot.plymouth.theme = "bgrt"; 
+  #boot.plymouth.logo = pkgs.fetchurl {
+  #  url = "https://raw.githubusercontent.com/RoseHobgoblin/RosebloomOS/main/assets/RosebloomEmblem.png";
+  #  sha256 = "0xdfjhplmg5qgnjmr70y93b5fza9kpzfcxhgmkn9sz52vlc5v195";
+  #};
 }

3e4d8ee63c4122a88e81999887d2cfb570e02c9c
RoseHobgoblin <earlofmagadan@Protonmail.com>
2024-07-12 01:08:49 +1200

    disable OS-prober so i can't access windows as easily to mod skyrim

--- system/boot.nix
+++ system/boot.nix
@@ -8,7 +8,7 @@
      efiSupport = true;
      #efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
      device = "nodev";
-     useOSProber = true;
+     #useOSProber = true;
      configurationLimit = 3;
   };
 };

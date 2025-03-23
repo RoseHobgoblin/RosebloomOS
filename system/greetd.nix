4986d047285c65e73cb537f31ba556d456d6f99f
RoseHobgoblin <EarlOfMagadan@protonmail.com>
2025-03-23 20:56:18 +1300

    amend

--- system/greetd.nix
+++ system/greetd.nix
@@ -12,7 +12,7 @@
       --time \
       --asterisks \
       --user-menu \
-      --theme border=red;text=red;prompt=red;time=red;action=red;button=red;container=black;input=white
+      --theme border=red;text=red;prompt=red;time=red;action=red;button=red;container=black;input=white \
       --cmd Hyprland
       '';
     };

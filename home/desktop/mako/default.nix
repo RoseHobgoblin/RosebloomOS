98e7ec1b9661f9d5640648c3f91cad2895a3249e
RoseHobgoblin <EarlOfMagadan@protonmail.com>
2025-09-21 11:56:12 +1200

    Mako update and config clean

--- home/desktop/mako/default.nix
+++ home/desktop/mako/default.nix
@@ -1,21 +1,16 @@
 { pkgs, config, theme, ... }: {
   services.mako = {
     enable = true;
-
-    #Rose Pine
-    #backgroundColor = "#1f1d2e";
-    #font = "M+1 Nerd Font 10";
-    #textColor = "#e0def4";
-    #borderColor = "#524f67";
-
-    backgroundColor = "#${theme.background0}";
-    font = "M+1 Nerd Font 10";
-    textColor =  "#${theme.foreground}";
-    borderColor =  "#${theme.secondary}";
-    borderSize = 2;
-    width = 400;
-    height = 600;
-    maxVisible = 5;
+    settings = {
+      backgroundColor = "#${theme.background0}";
+      font = "M+1 Nerd Font 10";
+      textColor =  "#${theme.foreground}";
+      borderColor =  "#${theme.secondary}";
+      borderSize = 2;
+      width = 400;
+      height = 600;
+      maxVisible = 5;
+    };    
   };
 }
 
